49cd57ca3f835e23483c2a8f0b6ae778acb66ae4
Delphine <dbsecretaire@gmail.com>
2025-10-29 07:54:04 +0100

    connexion

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL_BACKEND; // Remplacez par l’URL de votre backend
+const API_URL = process.env.NEXT_PUBLIC_API_URL; // Remplacez par l’URL de votre backend
 
 export async function fetchCalls(filter: { byName: string; byPhone: string }) {
   const res = await fetch(`${API_URL}/calls?byName=${filter.byName}&byPhone=${filter.byPhone}`);
--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL_BACKEND;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 
 import Cookies from 'js-cookie';
