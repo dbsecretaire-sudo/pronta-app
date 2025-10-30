e92d79f439506d72f1dec79c6c3cf226dbe9572d
Delphine <dbsecretaire@gmail.com>
2025-10-30 08:46:27 +0100

    navbar

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-import { useRedirectIfLoggedIn } from '@/app/hook/useAuth';
-
 export default function RegisterPage() {
-  useRedirectIfLoggedIn();
-
   return (
     <div>
       <h1>Register Page</h1>
