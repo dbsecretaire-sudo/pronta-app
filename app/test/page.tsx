c39f789b57fa34948184b50a52734e554f34b906
Delphine <dbsecretaire@gmail.com>
2025-11-01 13:35:30 +0100

    authentification

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { useSession } from "next-auth/react";
 
-export default function Test() {
-  const { data: session } = useSession();
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
+export default function TestMinimal() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <div>Chargement...</div>;
+  if (!session) return <div>Non connecté</div>;
+
+  return (
+    <div>
+      <h1>Session valide</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    </div>
+  );
 }
