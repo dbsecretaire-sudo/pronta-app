d8bb7b77a7b7e2bd7bfacda833e939aa37aee489
Delphine <dbsecretaire@gmail.com>
2025-11-01 13:11:23 +0100

    authentification

--- app/Types/Components/Session/Session.tsx
+++ app/Types/Components/Session/Session.tsx
@@ -1,11 +1,16 @@
 import { User } from "@/app/Types/Users/index";
 
-export  interface Session {
-    user: {
-        id: string;
-        email: string;
-        name: string;
-    };
+export interface Session {
+  user: {
+    id: string;
+    email: string;
+    name: string;
+  };
+  auth?: {  // ✅ Optionnel
+    userId: string;
+    email: string;
+    name: string;
+  };
 }
 
 export interface CustomUser extends Omit<User, 'id' | "password_hash" | "role" | "payment_method" | "created_at" | "subsciption_plan" | "subscription_end_date" > {
