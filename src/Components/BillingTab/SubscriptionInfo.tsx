0b77ec70227f332c285fb292e79d99b8adffff53
Delphine <dbsecretaire@gmail.com>
2025-11-04 08:44:26 +0100

    relation service subscriptions

--- src/Components/BillingTab/SubscriptionInfo.tsx
+++ src/Components/BillingTab/SubscriptionInfo.tsx
@@ -59,6 +59,7 @@ console.log("subscriptionsWithService: ", subscriptionsWithService);
 
   return (
     <div className="bg-blue-50 p-4 rounded-lg">
+      {subscriptions.length > 0 ? (
         <>
           {subscriptionsWithService.map((subscription) => {
             <div key={subscription.id} className="space-y-3 p-4 bg-white rounded-lg shadow-sm mb-4">
@@ -107,7 +108,7 @@ console.log("subscriptionsWithService: ", subscriptionsWithService);
             </div>
           })}
         </>
-      {/* ) : (
+      ) : (
         <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-sm text-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-gray-300 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
@@ -116,8 +117,8 @@ console.log("subscriptionsWithService: ", subscriptionsWithService);
           <p className="text-sm text-gray-500 mt-1">
             Vous n'avez actuellement aucun abonnement actif.
           </p>
-        </div> )}*/}
-      
+        </div>
+      )}
     </div>
   );
 };
