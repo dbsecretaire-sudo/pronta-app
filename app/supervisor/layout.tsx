455758c9ced8c8f999a5469238ed10847fa23b5c
Delphine <dbsecretaire@gmail.com>
2025-11-22 08:17:56 +0100

    admin

--- app/supervisor/layout.tsx
+++ app/supervisor/layout.tsx
@@ -1,9 +1,5 @@
 // src/app/admin/layout.tsx
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import { getRoleByUserId } from "@/src/lib/api";
-import { getServerToken, verifyAndDecodeToken } from "@/src/lib/auth";
 import { getSession } from "next-auth/react";
 export const dynamic = 'force-dynamic';
 export default async function AdminLayout({
